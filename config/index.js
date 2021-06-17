0ad8c4ed28b639908290d76323678a190b45f4d5
SubhamPramanik <subhampramanik@gmail.com>
2021-06-17 22:05:12 +0530

    Changed port

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  PORT: process.env.PORT ? process.env.PORT : 8080,
+  PORT: process.env.PORT ? process.env.PORT : 5000,
   NODE_ENV: process.env.NODE_ENV ? process.env.NODE_ENV : "production",
   MONGODB_URI: process.env.MONGODB_URI
     ? process.env.MONGODB_URI
